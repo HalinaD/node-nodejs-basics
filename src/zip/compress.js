ebfe43875fd01f52232ee2535964a08b1e5e71ff
halinad <gaxin@list.ru>
2024-01-29 19:02:09 +0300

    feat: add zlib exercises

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToCompressed = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const compressedFilePath = 'archive.gz';
+
+    const inputReadStream = fs.createReadStream(fileToCompressed);
+    const compressedWriteStream = fs.createWriteStream(compressedFilePath);
+
+    const compressStream = zlib.createGzip();
+    inputReadStream.pipe(compressStream).pipe(compressedWriteStream);
 };
 
 await compress();
