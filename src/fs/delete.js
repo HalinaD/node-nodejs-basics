7a468ad0306ea94186c4a5691f1e580db7c037d5
halinad <gaxin@list.ru>
2024-01-29 15:35:49 +0300

    feat: add file system exercises

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+  
+    try {
+        await fs.unlink(filePath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const fileText = await fs.readFile(fileToRead, 'utf-8');
+        console.log(fileText);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
